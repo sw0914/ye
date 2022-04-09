0a230e2c47c21d904597704b8216b762bdbef602
sw0914 <90825147+sw0914@users.noreply.github.com>
2022-04-09 16:45:07 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "329b0992-b7e1-11ec-b909-0242ac120002",
                         "alterId": 0
                     }
                 ],
